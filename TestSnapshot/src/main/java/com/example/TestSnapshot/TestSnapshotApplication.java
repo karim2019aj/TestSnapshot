7957c1a66c62c28e9e3cd118f6366e0e88cb69f7
abdelkarim <47181650+karim2019aj@users.noreply.github.com>
2022-12-11 18:20:43 +0100

    Initial commit

---
+++ TestSnapshot/src/main/java/com/example/TestSnapshot/TestSnapshotApplication.java
@@ -0,0 +1,15 @@
+package com.example.TestSnapshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSnapshotApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TestSnapshotApplication.class, args);
+		System.out.println("Version 0.0.1 test");
+	}
+
+}
---
+++ TestSnapshot/src/test/java/com/example/TestSnapshot/TestSnapshotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestSnapshot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSnapshotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
